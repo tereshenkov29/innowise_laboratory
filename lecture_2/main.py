0d1c7084fb24fad506bc76422dc4aa65b7448002
Yakov Tereshenkov <50947571+tereshenkov29@users.noreply.github.com>
2025-11-20 03:04:05 +0300

    Refactor mini-profile generator (#4)

--- lecture_2/main.py
+++ lecture_2/main.py
@@ -1,16 +1,22 @@
-user_name = input("Enter your full name: ")
-birth_year_str = input("Enter your birth year: ")
-birth_year = int(birth_year_str)
-current_year = 2025
-current_age = current_year - birth_year
+def generate_profile(age: int) -> str:
+    """Determine a person's life stage.
 
-def generate_profile(age):
+    :param age: Current person's age.
+    :return: Name of a life stage.
+    """
     if 0 <= age <= 12:
         return "Child"
     elif 13 <= age <= 19:
         return "Teenager"
     elif 20 <= age:
         return "Adult"
+
+
+user_name = input("Enter your full name: ")
+birth_year_str = input("Enter your birth year: ")
+birth_year = int(birth_year_str)
+current_year = 2025
+current_age = current_year - birth_year
 
 hobbies = []
 
@@ -21,7 +27,9 @@ while True:
     else:
         break
 
-user_profile = {"name": user_name, "age": current_age, "stage": generate_profile(current_age), "hobbies": hobbies}
+life_stage = generate_profile(current_age)
+
+user_profile = {"name": user_name, "age": current_age, "stage": life_stage, "hobbies": hobbies}
 
 print("")
 print("---")
@@ -30,7 +38,7 @@ print(f"Name: {user_profile['name']}")
 print(f"Age: {user_profile['age']}")
 print(f"Life Stage: {user_profile['stage']}")
 
-if  len(user_profile["hobbies"]) > 0:
+if len(user_profile["hobbies"]) > 0:
     print(f"Favorite Hobbies ({len(user_profile['hobbies'])}):")
     for i in user_profile["hobbies"]:
         print(f"- {i}")
